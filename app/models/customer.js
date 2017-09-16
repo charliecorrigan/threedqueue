4c74c21ab3c999a17a25627160870e9cc4b0de39
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-09-16 08:29:31 -0600

    Create customer model/class

---
+++ app/models/customer.js
@@ -0,0 +1,20 @@
+const environment = process.env.NODE_ENV || "development"
+const configuration = require("../../knexfile")[environment]
+const database = require('knex')(configuration)
+
+class Customer {
+  constructor(){
+    this.u_id   = 0;
+    this.email  = "";
+  }
+
+  static findOne(email){
+    return database.raw(`SELECT * FROM customers WHERE email=(?)`, [email])
+  }
+
+  save(){
+    return database.raw('INSERT INTO customers (email, created_at) VALUES  (?, ?) RETURNING *', [this.email, new Date])
+  }
+}
+
+module.exports = Customer;
