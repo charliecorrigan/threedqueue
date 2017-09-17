dccc268e54576bb3ffc9af91d582ee06dca111c9
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-09-17 09:40:08 -0600

    Set url path based on environment

--- public/javascript/projects.js
+++ public/javascript/projects.js
@@ -1,9 +1,17 @@
+const environment = "development"
+// const environment = "production"
+
+const baseApiUrl = {
+  'production': "https://threedqueue.herokuapp.com/api/v1",
+  'development': "http://localhost:8080/api/v1"
+}
+
 $(document).ready(function(){
   renderProjects();
 });
 
 renderProjects = function(){
-  $.getJSON('http://localhost:8080/api/v1/projects/awaiting')
+  $.getJSON(baseApiUrl[environment] + '/projects/awaiting')
   .then(function(projects){
     projects.forEach(function(project){
       $("#projects-container").append("<p>" + project.created_at, project.email, project.name, project.preferred_color, project.id, project.customer_comments, project.file_path, project.email + "</p>")
